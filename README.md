7ea00719e0b360a299fa57bcdc74e1ac33afd4ad
zeee1 <miw5281@gmail.com>
2019-08-11 16:13:59 +0900

    20190811 +solve 1289

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SW_expert
+
+## for 코딩 테스트. SW Expert 문제풀이 연습장
--- se_1289/Solution.java
+++ se_1289/Solution.java
@@ -1,15 +1,17 @@
 package se_1289;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
+    public static void main(String[] args) throws FileNotFoundException {
+        //Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(new FileInputStream("sample_input_1289.txt"));
         int testcaseNumber = scanner.nextInt();
 
-        for(int i = 0 ; i < testcaseNumber; i++){
-            String bitStr = scanner.nextLine();
+        for(int i = 1 ; i <= testcaseNumber; i++){
+            String bitStr = scanner.next();
 
             int stringLength = bitStr.length();
             String initString = "";
@@ -17,12 +19,39 @@ public class Solution {
             for(int j = 0 ; j < stringLength; j++)
                 initString += "0";
 
+            String prevBitStr = "";
+            int count = 0;
+
+
             for(int j = 0 ; j < stringLength ; j++){
-                if(initString.charAt(j) == bitStr.charAt(j))
+                String tmp ="";
+                if(initString.charAt(j) == bitStr.charAt(j)){
+                    prevBitStr += bitStr.charAt(j);
                     continue;
+                }
 
+                if(initString.charAt(j) == '0'){
+                    tmp = prevBitStr;
+                    for(int k = j; k < stringLength; k++){
+                        tmp += '1';
+                    }
+                }   else{
+                    tmp = prevBitStr;
+                    for(int k = j; k < stringLength; k++){
+                        tmp += '0';
+                    }
+                }
 
+                initString = tmp;
+                count += 1;
+
+                if(tmp.equals(bitStr))
+                    break;
+
+                prevBitStr += bitStr.charAt(j);
             }
+
+            System.out.println("#"+i+" "+count);
         }
     }
 }
